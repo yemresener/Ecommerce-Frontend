74ef7023a6ac2611d558b8d77c7094a141d773a3
yemresener <nestertun@gmail.com>
2025-12-25 17:01:09 +0300

    Add createComment section and improve repsonsive layout for navbar and eachPage

--- src/app/pages/product/product-review/product-review.component.ts
+++ src/app/pages/product/product-review/product-review.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-product-review',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './product-review.component.html',
   styleUrl: './product-review.component.css'
 })
 export class ProductReviewComponent {
+  commentPopUp = 0;
+
+
+
+  rating=0;
+  hoverRating=0;
+
+  setRating(value:number){
+    this.rating=value;
+  }
+  setHover(value:number){
+    this.hoverRating=value;
+  }
+  clearHover(){
+    this.hoverRating=0;
+  }
 
 }
