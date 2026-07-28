3c2223f56da4fba90f3a15e961a8e5768e61f457
yemresener <nestertun@gmail.com>
2026-07-28 17:58:08 +0300

    chore(frontend): apply branding updates and final production tweaks

--- src/app/staticPages/about-us/about-us.component.ts
+++ src/app/staticPages/about-us/about-us.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-about-us',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css'
 })
