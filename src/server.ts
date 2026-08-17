4ffbe190133f738ee675f3a6072eeaa823ea6a6e
yemresener <nestertun@gmail.com>
2026-08-18 00:01:41 +0300

    refactor(server): add production domains to SSR allowedHosts

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const indexHtml = join(serverDistFolder, 'index.server.html');
 import { RESPONSE_TOKEN } from './app/core/tokens/response.token';
 const app = express();
 const commonEngine = new CommonEngine({
-  allowedHosts:['localhost']
+  allowedHosts: ['localhost', 'gurmepet.com.tr', 'www.gurmepet.com.tr', 'admin.gurmepet.com.tr']
 });
 
 /**
